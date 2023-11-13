ae235d17bb3bef73673b7682d87c9120c8bc5fe9
Albin Holmqvist <150133846+albinholmqvist@users.noreply.github.com>
2023-11-13 15:38:10 +0100

    JS ändringar

--- hemsida.js
+++ hemsida.js
@@ -31,3 +31,60 @@ if (screenWidth < 768) {
     // Återställ till standardlayout för större skärmar
 }
 
+
+
+const orderDisplay = document.getElementById('orderDisplay');
+const searchInput = document.getElementById('searchInput');
+const csvDataDiv = document.getElementById('csvData');
+
+let ordersData = [];
+
+// Funktion för att läsa in CSV-fil
+function loadCSV() {
+    fetch('ordrar.csv') // Hämta CSV-filen
+        .then(response => response.text())
+        .then(data => {
+            const rows = data.split('\n'); // Dela upp rader
+            const headers = rows[0].split(','); // Anta att första raden är rubrikerna
+            for (let i = 1; i < rows.length; i++) {
+                const currentRow = rows[i].split(',');
+                if (currentRow.length === headers.length) {
+                    const order = {};
+                    for (let j = 0; j < headers.length; j++) {
+                        order[headers[j]] = currentRow[j]; // Skapa objekt med rubrikerna som nycklar
+                    }
+                    ordersData.push(order);
+                }
+            }
+            createOrderButtons(ordersData); // Anropa funktion för att skapa knappar
+        })
+        .catch(error => {
+            console.error('Det gick inte att läsa in CSV-filen', error);
+        });
+}
+
+// Funktion för att skapa knappar
+function createOrderButtons(data) {
+    orderDisplay.innerHTML = '';
+
+    data.forEach(order => {
+        const button = document.createElement('button');
+        button.textContent = `Order: ${order.Ordernummer}, Kund: ${order.Kund}, Adress: ${order.Adress}`;
+        orderDisplay.appendChild(button);
+    });
+}
+
+// Filtreringsfunktion
+searchInput.addEventListener('input', () => {
+    const searchValue = searchInput.value.toLowerCase();
+
+    const filteredOrders = ordersData.filter(order =>
+        order.Ordernummer.includes(searchValue) ||
+        order.Adress.toLowerCase().includes(searchValue) ||
+        order.Kund.toLowerCase().includes(searchValue)
+    );
+
+    createOrderButtons(filteredOrders);
+});
+
+loadCSV(); // Ladda in CSV-filen vid start
